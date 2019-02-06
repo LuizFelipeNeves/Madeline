3c8a351d1d9ae344442b5df22d4f08d4f4610dd3
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-02-06 19:58:33 -0200

    Update gerar_planilha.py

--- gerar_planilha.py
+++ gerar_planilha.py
@@ -52,7 +52,6 @@ def criarmatriz():
 def gravar(matriz):
     arquivotxt = './DADOS/OUTPUT_GL/' + str(ano) + '/TabMGLGLB_Diar.' + str(ano) + format(mes, '02d') + '.txt'
     arquivo = open(arquivotxt, 'w', encoding="ansi")
-    string_final= []
     espaco = ' '
     header = '%ID Lat Lon Alt Dono 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31'
     arquivo.write(header + espaco + '\n')
@@ -71,6 +70,7 @@ def gravar(matriz):
         arquivo.write(string + '\n')
     arquivo.close()
 
+ano = int(input('Insira o ano:'))
 for i in range(1, 13):
     mes = i
     inicio = timeit.default_timer()
