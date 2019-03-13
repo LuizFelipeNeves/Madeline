45d92fc23f3226ac750b43b1780db10319fa18b1
Luiz Felipe das Neves Lopes <luizfelipe2310@gmail.com>
2019-03-13 16:59:53 -0300

    *

--- dispersao.py
+++ dispersao.py
@@ -3,24 +3,24 @@ import pandas as pd
 import numpy as np
 #import csv
 
-sigla = 'SMS'
-ano = 2017
+sigla = 'Alta_Floresta'
+ano = 2018
 
 # CPA, PTR, NAT, SLZ, SMS
 
-def dispersao(posicao):
+def dispersao():
     global silga, ano, mes
-    estacoes = [[12, 1, 2], [3, 4, 5], [6, 7, 8 ], [9, 10, 11]]
-    cor=['blue', 'red', 'green']
+    estacoes = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10, 11, 12]
+    cor=['blue', 'red', 'green', 'yellow', 'orange', 'brown', 'gray', 'c','pink', 'lawngreen', 'navy' ]
     meses=['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
-    plt.figure(posicao+1)
+    plt.figure(1)
     plt.title("Rede Sonda - " + sigla + str(ano)[-2:] + " - Dispersão")
     plt.ylabel('Modelo GL')
     plt.xlabel('Sonda')
-
-    for j in range(3):
-        mes = estacoes[posicao][j]
+    plt.plot([0, 400], [0, 400], 'black')
+    
+    for mes in range(12):
         arquivotxt = './DADOS/TXT/' + str(ano) + '/' + sigla + '/' + sigla + str(ano)[-2:] + format(mes, '02d') + '.txt'
 
         try:
@@ -32,16 +32,15 @@ def dispersao(posicao):
                 if(sonda[i] > 1600) or sonda[i] == -999: sonda[i]=None
                 if(GL[i] > 1600) or GL[i] == -999: GL[i]=None
 
-            plt.plot([0, 400], [0, 400], 'black')
-            plt.scatter(sonda, GL, c=cor[j], label=meses[mes-1], alpha=0.5)
-            plt.scatter(media(sonda), media(GL), marker='s', c=cor[j], alpha=1.0)
+            plt.scatter(sonda, GL, c=cor[mes-1], label=meses[mes-1], alpha=0.5)
+            plt.scatter(media(sonda), media(GL), marker='s', c=cor[mes-1], alpha=1.0)
             
         except FileNotFoundError: pass
             
     plt.xlim(0, 400)
     plt.ylim(0, 400)
     plt.legend(loc='upper left') #bbox_to_anchor=(0.5, 1), loc='upper left', borderaxespad=0.
-    plt.savefig('./DADOS/IMAGENS/' + sigla + '-Dispersao' + str(posicao+1) + '.png')
+    plt.savefig('./DADOS/IMAGENS/' + sigla + '-Dispersao' + 'Final' + '.png')
 
 # Soma todos os elementos de um array
 def media(array):
@@ -53,8 +52,7 @@ def media(array):
             count += 1
     return soma/(count-1)
 
-for posicao in range(4):
-    dispersao(posicao)
+dispersao()
 
 #dispersao(1)
 #dispersao(2)
